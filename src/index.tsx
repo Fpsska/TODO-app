b1c083dd6baad7bdf9348ee894789dbc88813aee
Fpsska <fpsska1337@gmail.com>
2022-08-28 13:38:30 +0300

    feat: create serviceWorkerRegistration.ts, registration in index.tsx component & confifure manifest.json file

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { store } from './store/store';
 
 import Layout from './app/components/Layout/Layout';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 // /. imports
@@ -22,6 +23,12 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register('');
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
